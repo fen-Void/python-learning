f0896e2354c20cecf73a6321a6b790036de0bad1
FenVoid <3000666@protonmail.com>
2026-01-26 22:54:31 +0530

    Update

--- Python-basic/nested.py
+++ Python-basic/nested.py
@@ -1,15 +1,34 @@
 # Example 2: Ticket Price Calculator (Age + Membership)
 
-age = int(input("Age bata: "))
-is_member = input("Gym/Cinema member hai? (yes/no): ").lower() == "yes"
+# ge = int(input("Age bata: "))
+# is_member = input("Gym/Cinema member hai? (yes/no): ").lower() == "yes"
 
-if age >= 18:
-    if is_member:
-        print("Adult member → Ticket ₹150")
+# if age >= 18:
+#     if is_member:
+#         print("Adult member → Ticket ₹150")
+#     else:
+#         print("Adult non-member → Ticket ₹250")
+# else:
+#     if is_member:
+#         print("Child member → Ticket ₹80")
+#     else:
+#         print("Child non-member → Ticket ₹120")
+
+
+# Number Category Checker (Nested)
+
+num = int(input("Enter your No: "))
+
+if num > 0 :
+    if num%2==0:
+        print("Positive Even")
     else:
-        print("Adult non-member → Ticket ₹250")
+        print("Positive odd")
+elif num <0 :
+    if num%2==0:
+        print("Negative even")
+    else:
+        print("Negative odd")
+
 else:
-    if is_member:
-        print("Child member → Ticket ₹80")
-    else:
-        print("Child non-member → Ticket ₹120")
+    print ("Zero")
